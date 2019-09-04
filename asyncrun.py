54bdd3a61d4f4d3595c693ee0c87685ae603a973
Kevin <Kevin.Striek@gmail.com>
2019-09-04 15:43:22 +0200

    Fix conversion of arguments to strings

--- asyncrun.py
+++ asyncrun.py
@@ -1,26 +1,36 @@
 import asyncio
 import logging
-from pathlib import PurePath
+from typing import List
 
 log = logging.getLogger()
 
-async def asyncrun(cmd, *args):
+
+async def convert_args_to_str(*args) -> List[str]:
+    newargs: List[str] = []
+    for arg in args:
+        if not isinstance(arg, str):
+            newargs.append(str(arg))
+        else:
+            newargs.append(arg)
+    return newargs
+
+
+async def asyncrun(cmd: str, *args) -> str:
     """Runs the given command, connects stdout to the current shell and waits until it is completed"""
     log.info(f"Running cmd {cmd} with {list(args)}")
-    for arg in args:
-        if isinstance(arg, PurePath):
-            arg = str(arg)
-    proc = await asyncio.subprocess.create_subprocess_exec(cmd, *args, stdout=asyncio.subprocess.PIPE)
+    newargs: List[str] = await convert_args_to_str(*args)
+    proc = await asyncio.subprocess.create_subprocess_exec(cmd, *newargs, stdout=asyncio.subprocess.PIPE)
     out = await proc.stdout.read()
     out = out.decode().rstrip()
     await proc.wait()
     return out
 
 
-async def asyncrun_quiet(cmd, *args):
+async def asyncrun_quiet(cmd: str, *args) -> str:
     """Runs the given command quietly and waits until it is completed."""
     log.debug(f"Quietly Running cmd {cmd} with {list(args)}")
-    proc = await asyncio.subprocess.create_subprocess_exec(cmd, *args, stdout=asyncio.subprocess.PIPE,
+    newargs: List[str] = await convert_args_to_str(*args)
+    proc = await asyncio.subprocess.create_subprocess_exec(cmd, *newargs, stdout=asyncio.subprocess.PIPE,
                                                            stderr=asyncio.subprocess.DEVNULL)
     out = await proc.stdout.read()
     out = out.decode().rstrip()
