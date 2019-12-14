026693e2d54e9bc1953acd34164c53fcfa6c3e1d
Kevin Striek <Kevin.Striek@gmail.com>
2019-12-14 22:09:16 +0100

    Add AutoGUI script

---
+++ auto_gui.py
@@ -0,0 +1,44 @@
+import pyautogui
+import sys
+import time
+
+pyautogui.PAUSE = 0.7
+
+
+def main():
+    try:
+        time.sleep(5)
+        while True:
+            pyautogui.moveTo(1265, 196)
+            pyautogui.click()
+            pyautogui.moveTo(961, 377)
+            pyautogui.click()
+            pyautogui.moveTo(1057, 394, duration=1.0)
+            time.sleep(5)
+            pyautogui.moveTo(943, 238)
+            pyautogui.click()
+            # time.sleep(1)
+            pyautogui.moveTo(673, 416)
+            pyautogui.click()
+            pyautogui.moveTo(729, 555)
+            pyautogui.click()
+            pyautogui.press('right')
+
+
+    except KeyboardInterrupt:
+        print('\n')
+
+
+def record_mouse():
+    try:
+        while True:
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr)
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print('\n')
+
+
+if __name__ == '__main__':
+    main()
